696ba95bb611b6e366c7d8fbfa9d0095689af269
happy <nawanihappy@gmail.com>
2021-01-15 14:48:39 +0530

    stage

--- stage/main.tf
+++ stage/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "h2ppy-terraform-state-dev-env"
+    bucket = "h2ppy-terraform-state-stage-env"
     key = "default-infrastructure"
     region = "us-east-1"
   }
