a13d96708c04f0380a3745e3810fe79cae9c3225
happy <nawanihappy@gmail.com>
2021-01-15 13:17:27 +0530

    the first change

---
+++ dev/nat.tf
@@ -0,0 +1,7 @@
+resource "aws_nat_gateway" "happy-dev-nat" {
+  allocation_id = aws_eip.happy-ip.id
+  subnet_id = aws_subnet.happy-dev-public-subnet-1a.id
+  tags = {
+    Name = "happy-dev-nats"
+  }
+}
---
+++ dev/vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "happy-dev-vpc" {
+  cidr_block = "10.10.0.0/16"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "false"
+  enable_classiclink = "false"
+  instance_tenancy = "default"
+  tags = {
+    Name = "happy-dev-vpc"
+  }
+}
+
+
+resource "aws_main_route_table_association" "happy-dev-vpc-main-rt-association" {
+  vpc_id = aws_vpc.happy-dev-vpc.id
+  route_table_id = aws_route_table.dev-rt-nat.id
+}
