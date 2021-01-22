9fafe8eb15ccbb82f29827c181f53ba4ac526082
happy <nawanihappy@gmail.com>
2021-01-22 16:33:46 +0530

    trying autosacling group policies

--- dev/try.tf
+++ dev/try.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "get_target_group_arn" {
   provisioner "local-exec" {
-    command = "aws elbv2 describe-target-groups --load-balancer-arn ${module.base.aws_elastic_beanstalk_environment_load_balancers[0]} --output text --query 'TargetGroups[?LoadBalancerArns==[`${module.base.aws_elastic_beanstalk_environment_load_balancers[0]}`]].TargetGroupArn'  > ${data.template_file.target_group_in_template.rendered}"
+    command = "aws elbv2 describe-target-groups --region us-east-1 --load-balancer-arn ${module.base.aws_elastic_beanstalk_environment_load_balancers[0]} --output text --query 'TargetGroups[?LoadBalancerArns==[`${module.base.aws_elastic_beanstalk_environment_load_balancers[0]}`]].TargetGroupArn'  > ${data.template_file.target_group_in_template.rendered}"
   }
   depends_on = [module.base]
 }
