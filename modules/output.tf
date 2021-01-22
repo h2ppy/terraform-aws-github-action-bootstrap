db6f63cb0e8ee84bbb99966c90cee6c8dc7217af
happy <nawanihappy@gmail.com>
2021-01-22 16:27:09 +0530

    trying autosacling group policies

---
+++ modules/output.tf
@@ -0,0 +1,58 @@
+output "aws_elastic_beanstalk_environment_load_balancers" {
+  value = aws_elastic_beanstalk_environment.chem101Env.load_balancers
+}
+
+output "aws_elastic_beanstalk_environment_autoscaling_groups" {
+  value = aws_elastic_beanstalk_environment.chem101Env.autoscaling_groups
+}
+
+output "aws_elastic_beanstalk_environment_launch_configurations" {
+  value = aws_elastic_beanstalk_environment.chem101Env.launch_configurations
+}
+
+output "aws_elastic_beanstalk_environment_all_settings" {
+  value = aws_elastic_beanstalk_environment.chem101Env.all_settings
+}
+
+
+output "aws_elastic_beanstalk_environment_id" {
+  value = aws_elastic_beanstalk_environment.chem101Env.id
+}
+
+output "aws_elastic_beanstalk_environment_application" {
+  value = aws_elastic_beanstalk_environment.chem101Env.application
+}
+
+output "aws_elastic_beanstalk_environment_arn" {
+  value = aws_elastic_beanstalk_environment.chem101Env.arn
+}
+
+output "aws_elastic_beanstalk_environment_cname" {
+  value = aws_elastic_beanstalk_environment.chem101Env.cname
+}
+
+output "aws_elastic_beanstalk_environment_cname_prefix" {
+  value = aws_elastic_beanstalk_environment.chem101Env.cname_prefix
+}
+
+
+output "aws_elastic_beanstalk_environment_endpoint_url" {
+  value = aws_elastic_beanstalk_environment.chem101Env.endpoint_url
+}
+
+output "aws_elastic_beanstalk_environment_platform_arn" {
+  value = aws_elastic_beanstalk_environment.chem101Env.platform_arn
+}
+
+
+output "aws_elastic_beanstalk_environment_poll_interval" {
+  value = aws_elastic_beanstalk_environment.chem101Env.poll_interval
+}
+
+
+output "aws_elastic_beanstalk_environment_queues" {
+  value = aws_elastic_beanstalk_environment.chem101Env.queues
+}
+output "aws_elastic_beanstalk_environment" {
+  value = aws_elastic_beanstalk_environment.chem101Env
+}
