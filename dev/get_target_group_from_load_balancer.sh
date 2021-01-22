7c9582f075a7a6e2b96b43bc75e50693cb3276e2
happy <nawanihappy@gmail.com>
2021-01-22 21:33:58 +0530

    trying autosacling group policies

--- dev/get_target_group_from_load_balancer.sh
+++ dev/get_target_group_from_load_balancer.sh
@@ -2,5 +2,5 @@ eval "$(jq -r '@sh "LOAD_BALANCER_ARN=\(.load_balancer_arn)"')"
 
 target=`aws elbv2 describe-target-groups --region us-east-1 --load-balancer-arn $LOAD_BALANCER_ARN --output json --query "TargetGroups[?LoadBalancerArns==[\`$LOAD_BALANCER_ARN\`]]"`
 
-${target[0]}
+$target[0]
 #{"target":$target}
