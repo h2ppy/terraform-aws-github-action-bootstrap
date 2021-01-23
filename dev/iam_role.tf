7a17249de881ccbc8c2c1187351be5885e9001f9
happy <nawanihappy@gmail.com>
2021-01-23 20:18:33 +0530

    trying autosacling group policies

--- dev/iam_role.tf
+++ dev/iam_role.tf
@@ -1,4 +1,27 @@
+data "aws_iam_policy" "AWSElasticBeanstalkWebTier" {
+  arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+data "aws_iam_policy" "AWSElasticBeanstalkMulticontainerDocker" {
+  arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
+}
+data "aws_iam_policy" "AWSElasticBeanstalkWorkerTier" {
+  arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
+}
 
+resource "aws_iam_role_policy_attachment" "AWSElasticBeanstalkMulticontainerDocker-policy-attach" {
+  role       = aws_iam_role.dev-ec2-role
+  policy_arn = data.aws_iam_policy.AWSElasticBeanstalkMulticontainerDocker
+}
+
+resource "aws_iam_role_policy_attachment" "AWSElasticBeanstalkWebTier-policy-attach" {
+  role       = aws_iam_role.dev-ec2-role
+  policy_arn = data.aws_iam_policy.AWSElasticBeanstalkWebTier
+}
+
+resource "aws_iam_role_policy_attachment" "AWSElasticBeanstalkWorkerTier-policy-attach" {
+  role       = aws_iam_role.dev-ec2-role
+  policy_arn = data.aws_iam_policy.AWSElasticBeanstalkWorkerTier
+}
 resource "aws_iam_role" "dev-ec2-role" {
   name = "dev-ec2-role"
   assume_role_policy = <<EOF
