e4ea504dcac0ee4ce04d5f5eb45eae58d8ddd6cb
happy <nawanihappy@gmail.com>
2021-01-25 11:53:19 +0530

    trying autosacling group policies

--- dev/ec2_instance.tf
+++ dev/ec2_instance.tf
@@ -4,10 +4,10 @@ module "bastion-host" {
 
   name                   = "bastion-host-dev"
   instance_count         = 1
-
   ami                    = "ami-04b9e92b5572fa0d1"
   instance_type          = "t2.micro"
   key_name               = "user1"
+  associate_public_ip_address = true
   monitoring             = false
   vpc_security_group_ids = [aws_security_group.chem101-dev-bastion-security-group.id]
   subnet_id              = aws_subnet.happy-dev-public-subnet-1a.id
